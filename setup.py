c14135698271927986f2199928183f159a0ac9f8
Nimit-Khurana <nimbukhur@gmail.com>
2020-12-18 22:07:00 +0530

    make pypi package

--- setup.py
+++ setup.py
@@ -1,17 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-	name="pyWallet",
+	name="passwallet",
 	version="1.0",
 	author="Richie",
 	author_email="nimitkhuranaa@gmail.com",
 	description="Password Wallet",
 	url="https://github.com/Nimit-Khurana/pyWallet",
-	packages=find_packages(),
-	classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: Linux",
-    	],
-    python_requires='>=3.6',
+	packages=["passwallet","tests"],
 )
